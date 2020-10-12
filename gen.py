a440fbc1286f3099912fffe4534b25d10278a5cb
Elijah1111 <elijahmtcastle@gmail.com>
2020-10-12 12:56:32 -0600

    Added input generator

---
+++ gen.py
@@ -0,0 +1,16 @@
+import random
+
+n = int(input("Input N: "))
+points = {}
+for i in range(n):
+    tmp = (random.randint(0,1000),random.randint(0,1000))
+    points[random.randint(0,1000)] = tmp
+
+
+with open("Input.txt",'w') as f:
+    print(len(points),file=f)
+    print(random.randint(2,10),file=f)
+
+    for value in points:
+        print(value,points[value][0],points[value][1],file=f)
+
